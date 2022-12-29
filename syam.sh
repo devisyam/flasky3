df118966ac4e663a24ac50be11bdf8b9301d6c88
root <root@ip-172-31-11-38.us-east-2.compute.internal>
2022-12-29 10:48:08 +0000

    dev

---
+++ syam.sh
@@ -0,0 +1,16 @@
+cd ~
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
+sudo apt -y install nodejs
+ npm install -g @angular/cli@12
+ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+ source ~/.bashrc
+ nvm list-remote
+  nvm install v16.13.0
+  nvm use v16.13.0
+      ng -v
+     ng version
+     git clone https://github.com/devisyam/cms-ui.git
+     ll
+    cd cms-ui/
+    npm install --save-dev @angular-devkit/build-angular
+    ng build
